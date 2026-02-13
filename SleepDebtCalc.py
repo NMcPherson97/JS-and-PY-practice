5664ab7c4b1e9a258568dcf1b5c2c310eec2d61f
NMcPherson97 <nmcpherson215@gmail.com>
2026-02-12 18:38:30 -0600

    Attempt with only one function. Still running into iteration issues.

--- SleepDebtCalc.py
+++ SleepDebtCalc.py
@@ -1,6 +1,7 @@
 #Slightly more familiar with python. 
 #Instead of writing if/else statement like SleepDebtCalc.js, create dictionary{day:ideal hours of sleep}
-day_sleep_ideal = {'Monday':8, 
+day_sleep_ideal = {
+                    'Monday':8, 
                    'Tuesday':8,
                    'Wednesday':8,
                    'Thursday':8,
@@ -8,43 +9,42 @@ day_sleep_ideal = {'Monday':8,
                    'Saturday':10,
                    'Sunday':10}
 
-#Create function to iterate (ideal)sleep hour values 
-def get_sleep_hours(day):
-    for hours in day_sleep_ideal.values():
-        return hours
+ideal_hours = list(day_sleep_ideal.values())
+print(ideal_hours)
 
 #Actual sleep hours dictionary
 day_sleep_actual = {
-    'Monday':3,
-    'Tuesday':5,
-    'Wednesday':4,
-    'Thursday':8,
-    'Friday':6,
-    'Saturday':10,
-    'Sunday':9}
+                'Monday':3,
+                'Tuesday':5,
+                'Wednesday':4,
+                'Thursday':8,
+                'Friday':6,
+                'Saturday':10,
+                'Sunday':9}
 
+actual_hours = list(day_sleep_actual.values())
+print(actual_hours)
+ 
 
 #Fix sleep_debt function. Keeps printing 'you need to get 5 more hours of sleep' regardless of argument.
 def sleep_debt(day):
-    sleep_actual = list(day_sleep_actual.values())
-    sleep_ideal = list(day_sleep_ideal.values())
-    for hours in sleep_actual:
-        for h in sleep_ideal:
-            if hours == h:
-                return "You got enough sleep!"
-            elif hours <= h:
-                return f"You need to get {h - hours} more hours of sleep!"
-            elif h > hours:
-                return f"You overslept by {hours - h} hours."
-# Nested loop attempt 1:
-    # for hours in day_sleep_actual.values():
-    #     for h in day_sleep_ideal.values():
-    #         if hours == h:
-    #             return "Congratulations on getting enough sleep!"
-    #         elif hours <= h:
-    #             return f"You need to get {h - hours} more hours of sleep!"
-    #         elif hours > h:
-    #             return f"You overslept by {h} hours!"
+    for hours in ideal_hours:
+        for h in actual_hours:
+            if actual_hours[h:] == ideal_hours[hours:]:
+                return 'You got enough sleep last night!'
+            elif actual_hours[h:] < ideal_hours[hours:]:
+                return f'You need to get {hours - h} more hours of sleep!'
+            elif actual_hours[h:] > ideal_hours[hours:]:
+                return 'You overslept!'
+  
+
+print(sleep_debt('Monday'))
+
+
+   
+
+
+
 
 
 
